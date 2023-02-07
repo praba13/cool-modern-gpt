f554a5c2112de82aa317a0d8f0e0b9125e9a8e8d
Praba <prama1303@gmail.com>
2023-02-07 12:23:46 +0100

    file structure is done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { Brand, CTA, Navbar } from './components';
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3
+} from './containers';
 function App() {
   return (
     <div className='App'>
-      <h1>Hi</h1>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
